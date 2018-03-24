a23048a07668ec5a61d801f62fecfb3cbe19886e
Yagnik Patel <yagnikpatel1439@gmail.com>
2018-03-24 11:32:23 +0530

    New Interactive Slider

---
+++ ipywidgets.py
@@ -0,0 +1,10 @@
+from ipywidgets import interactive
+import ipywidgets as widgets
+from IPython.display import display
+
+def func(a,b):
+	display(a**b)
+	return a**b
+
+out = interactive(func,a=10,b=15)
+display(out) 
