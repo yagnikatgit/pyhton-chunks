cba48f8cb350c2fb444d309cc1cf9bac4ffff326
Yagnik Patel <35323575+yagnikatgit@users.noreply.github.com>
2018-06-21 12:28:17 +0530

    Updated to python 3.x

--- Prime.py
+++ Prime.py
@@ -1,9 +1,21 @@
+''' 
+
+Checks whether the given number is prime or not. 
+
+'''
+
 def is_prime(x):
+    
+    # If given number is less than 2 function returns false as 0 & 1 are not primes.
     if x < 2:
         return False
+    
     else:
+        # Iterate through 2 to number-1.
         for n in range(2, x-1):
+            
+            # Checks for positive divisor
             if x % n == 0:
                 return False
+            
         return True
-
