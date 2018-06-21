b256853465a4684846a4fca4b54a82579874c37e
Yagnik Patel <35323575+yagnikatgit@users.noreply.github.com>
2018-06-21 12:09:00 +0530

    Updated to python 3.x

--- DigitSum.py
+++ DigitSum.py
@@ -1,8 +1,22 @@
+''' 
+
+Results into sum of all digits. 
+
+'''
+
 def digit_sum(x):
     total = 0
+    
+    # While there is atleast 1 digit loop will continue.
     while x > 0:
+        
+        # Mudulo by 10 gives last digit of the given number.
         total += x % 10
+        
+        # Use of classic division (e.g. 5//2 = 2)
+        # Gives all digits except last one.
         x = x // 10
-        print x
+    
     return total
-print digit_sum(223)
+
+print(digit_sum(1439))
