39616705802566a9ba7719cae7b363521809bc7e
Yagnik Patel <35323575+yagnikatgit@users.noreply.github.com>
2018-06-23 09:38:46 +0530

    Update quick_sort.py

--- quick_sort.py
+++ quick_sort.py
@@ -1,10 +1,26 @@
+''' 
+
+Quick sort using list comprehension. 
+
+'''
+
 def quick_sort(lst):
+    # Checks length of list.
     if len(lst) <= 1:
         return(lst)
+    
+    # Sets pivot in the middle
     pivot= lst[(len(lst) // 2)]
+    
+    # Sort numbers less than pivot.
     left = [x for x in lst if x < pivot]
+    
     middle = [x for x in lst if x == pivot]
+    
+    # Sort numbers greater than pivot.
     right = [x for x in lst if x > pivot]
+    
+    # Merge three lists.
     return quick_sort(left) + quick_sort(middle) + quick_sort(right)
 
 print(quick_sort([5,4,3,8,7,1,2,6]))
