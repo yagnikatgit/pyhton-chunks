acd73ef1d564122c75de24f92fc0d22ea52fe4a9
Yagnik Patel <35323575+yagnikatgit@users.noreply.github.com>
2018-06-23 09:29:53 +0530

    Updated to python 3.x

--- garbled.py
+++ garbled.py
@@ -1,4 +1,12 @@
+''' 
+
+Finds secret message from garbled string. 
+
+'''
+
 garbled = "!XeXgXaXsXsXeXmX XtXeXrXcXeXsX XeXhXtX XmXaX XI"
 
+# Step size -2 reveals real message.
 message = garbled[::-2]
-print message
+
+print(message)
